a22c45756fee39f9ae6a2db59a082ff7552634f6
Avanish-git <55701669+Avanish-git@users.noreply.github.com>
2019-10-18 15:26:01 +0530

    Update SmallestSubstring1.java

--- SmallestSubstring1.java
+++ SmallestSubstring1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SmallestSubstring1 {
+public class SmallestSubString1 {
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
